2c71d7d5eed63edfa9cf93c4a2ee3966342f5909
D.N,Chen <dnchenbeta@gmail.com>
2017-12-14 20:37:31 +0800

    Update bys.py

--- bys.py
+++ bys.py
@@ -1,26 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Dec  8 21:10:43 2017
-
-@author: DnChen
+题目：利用贝叶斯框架估计共有10个门，主持人打开3个空门的蒙提霍尔问题。
 """
 
 # -*- coding: utf-8 -*-
-
-#多面筛子&火车头问题的贝叶斯处理框架
-
-#E表示证据集合，data表示证据中的一项
-#hypos表示假设集合，hypo表示假设的一项
-#prob表示概率的字典,存储格式为{'hypo1':0.01,'hypo2':0.02,...}
 import random
 
-def get_value(n):
+def get_value(n): #计算阶乘
     if n==1:
         return n
     else:
         return n * get_value(n-1)
         
-def C(n,m):
+def C(n,m): #计算组合数
      first = get_value(n)
      second = get_value(m)
      third = get_value((n-m))
@@ -35,12 +27,13 @@ def pre(hypos): #先验概率P(Hx)，x=0,1,2...
     return probs
 
 def LikeliHood(data,hypo): #一个证据data出现时，计算P(data|Hx),x=0,1,2...
+    re = 0
     if hypo == data or data == e1:
         re=0
-    elif hypo!= data and hypo == e1:
-        re= C(8, 2)/C(10, 3)
-    elif hypo!= data and hypo!= e1:
-        re= C(7, 2)/C(10, 3)
+    if hypo!= data and hypo == e1:
+        re= C(8, 2)/C(9, 3)
+    if hypo!= data and hypo!= e1:
+        re= C(7, 2)/C(8, 3)
     return re
 
 def update(evidence,probs,hypos):  #更新后验概率，P(data|Hx)*P(Hx),x=0,1,2...
@@ -60,6 +53,6 @@ def run(evidence,hypos):
     return probs
 
 H = [1,2,3,4,5,6,7,8,9,10]
-e1 = random.choice(H)
-E = [2,3,4]
-print('选择：', e1, run(E, H))
+E = random.sample(H, 3)
+e1 = random.choice(list(set(H).difference(set(E))))
+print('选择：', e1, "开门：", E, run(E, H))
